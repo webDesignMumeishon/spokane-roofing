b39d29dda36cbe3ce857d9c35047805aadb0e59b
webDesignMumeishon <muma.sanmartin2011@gmail.com>
2024-07-18 20:40:12 -0300

    add contractors

--- src/config.js
+++ src/config.js
@@ -3,6 +3,6 @@ module.exports.SITE = {
   origin: 'https://spokaneroofing.co/',
   basePathname: '/',
   trailingSlash: false,
-  title: 'Spokane Roofing - Your Trusted Roofing Experts',
+  title: 'Spokane Roofing | Your Trusted Roofing Experts | Roofing Contractors',
   description: `Contact us today at (509) 294-3442 Spokane Roofing! Our expert roofers provide replacement and installation services.`,
 };
